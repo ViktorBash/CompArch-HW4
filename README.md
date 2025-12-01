71b809c4620ad759da5da4851af325ec80efb9ea
ViktorBash <basharkevichv@gmail.com>
2025-12-01 18:18:27 -0500

    Got basic merge sort working --> Reads/Writes files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # CompArch-HW4
 HW4 code for computer architecture
+
+## How to generate your data:
+Don't add data files to git. The count=10 example is in git for clarity, but others
+are too large for git :C
+```
+python gen_data.py --count=1000
+python gen_data.py --count=10000
+python gen_data.py --count=100000
+python gen_data.py --count=1000000
+python gen_data.py --count=10000000
+python gen_data.py --count=100000000  # Will take ~60s to run, generates ~10GB file
+```
+
+## How to run the program
+```
+make
+./sorting data/random-10.txt
+```
+
+## Where the output is
+```
+sorted_data/random-10.txt
+```
--- sorting.c
+++ sorting.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 void merge(uint32_t *arr, size_t left, size_t mid, size_t right) {
     int i, j, k;
     int n = mid - left + 1;
     int m = right - mid;
 
-    int leftArr[n], rightArr[m];
+    // TODO: This can be optimized, or at least not made a stack variable
+    // Cannot handle 1GB sized file right now
+    uint32_t leftArr[n], rightArr[m];
 
     for (j = 0; j < m; j++)
         rightArr[j] = arr[mid + 1 + j];
@@ -68,20 +71,87 @@ void sort_array(uint32_t *arr, size_t size) {
     merge_sort(arr, 0, size - 1);
 }
 
-int main() {
-    //Initialise the array
-    int size = 100;
-
-    uint32_t *sorted_arr = malloc(size * sizeof(uint32_t)); // Allocate memory for the sorted array
-    
-    // Sort the copied array
-    sort_array(sorted_arr, size);
-
-    // Print the sorted array
-    for (int i = 0; i < size; ++i) {
-        printf("%d\n", sorted_arr[i]);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
     }
 
+    char *filename = argv[1];
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Can't open file, error");
+        return 1;
+    }
+
+    // We alloc size=1000 and then double with realloc as we need to
+    int size = 0;
+    int capacity = 1000;
+    uint32_t *sorted_arr = malloc(capacity * sizeof(uint32_t));
+    if (sorted_arr == NULL) {
+        perror("Failed to allocate initial memory");
+        fclose(file);
+        return 1;
+    }
+
+    while (fscanf(file, "%u", &sorted_arr[size]) == 1) {
+        size++;
+        if (size >= capacity) {
+            capacity *= 2;
+            // Double the size of sorted_arr
+            uint32_t *temp = realloc(sorted_arr, capacity * sizeof(uint32_t));
+            if (temp == NULL) {
+                perror("Failed to reallocate memory");
+                free(sorted_arr);
+                fclose(file);
+                return 1;
+            }
+            sorted_arr = temp;
+        }
+    }
+    fclose(file);
+
+    // Print out the input (unsorted)
+    //    for (int i = 0; i < size; ++i) {
+    //        printf("%u\n", sorted_arr[i]);
+    //    }
+
+    // Sort the array
+    sort_array(sorted_arr, size);
+
+    // Print out the result (sorted)
+    //    for (int i = 0; i < size; ++i) {
+    //        printf("%u\n", sorted_arr[i]);
+    //    }
+
+    // Construct output file path
+    char output_filepath[256]; // Max 256 filename length
+    char *input_basename = strrchr(filename, '/');
+    if (input_basename == NULL) {
+        input_basename = strrchr(filename, '\\');
+    }
+    if (input_basename == NULL) {
+        input_basename = filename;
+    } else {
+        input_basename++;
+    }
+    snprintf(output_filepath, sizeof(output_filepath), "sorted_data/%s", input_basename);
+
+    FILE *output_file = fopen(output_filepath, "w");
+    if (output_file == NULL) {
+        perror("Error opening output file");
+        free(sorted_arr);
+        return 1;
+    }
+
+    // Populate the output file
+    for (int i = 0; i < size; ++i) {
+        fprintf(output_file, "%u\n", sorted_arr[i]);
+    }
+
+    // Cleanup time
+    fclose(output_file);
+    free(sorted_arr);
     return 0;
 }
 
